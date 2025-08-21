864f2fb7358d1db9923962e3e50981ad2dfdf49f
Tilman Kurmayer <tilman.kurmayer@gmail.com>
2025-08-21 16:22:42 +0200

    refactor(utils): :fire: Remove Error associated type from RandomNumberGenerator trait

--- src/utils/random.rs
+++ src/utils/random.rs
@@ -3,14 +3,12 @@ use rand::TryRngCore;
 use rand::rngs::OsRng;
 
 pub trait RandomNumberGenerator {
-    type Error;
     fn generate_nonce_92bit(&mut self) -> NimbusResult<Vec<u8>>;
     fn generate_nonce_192bit(&mut self) -> NimbusResult<Vec<u8>>;
     fn generate_random_u64(&mut self) -> NimbusResult<u64>;
 }
 
 impl RandomNumberGenerator for OsRng {
-    type Error = NimbusError;
     fn generate_nonce_92bit(&mut self) -> NimbusResult<Vec<u8>> {
         let mut nonce = vec![0; 12];
         self.try_fill_bytes(&mut nonce)
